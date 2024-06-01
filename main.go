bf324fae096f2877d33c6ef1428b347d09d21693
yunho <wjse213@coontec.com>
2024-06-01 17:45:38 +0900

    add cli

--- main.go
+++ main.go
@@ -3,10 +3,52 @@ package main
 import (
 	"coin/explorer"
 	"coin/rest"
+	"flag"
+	"fmt"
+	"os"
 )
 
-func main() {
-	go explorer.Start(4001)
-	rest.Start(4000)
+func useage() {
+	fmt.Printf("welcome to 노마드 코드\n\n")
+	fmt.Printf("use following command\n\n")
+	fmt.Printf("explorer:	start html explorer\n")
+	fmt.Printf("rest:		start rest api (recommand)\n\n")
+	os.Exit(0)
 
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		useage()
+	}
+	// rest := flag.NewFlagSet("restr", flag.ExitOnError)
+	// portFlag := rest.Int("port", 4000, "set the server port")
+	// switch os.Args[1] {
+	// case "explorer":
+	// 	fmt.Println("start explorer")
+	// case "rest":
+	// 	rest.Parse(os.Args[2:])
+	// 	fmt.Println("start rest api")
+
+	// default:
+	// 	useage()
+	// }
+	// if rest.Parsed() {
+	// 	fmt.Print("test")
+	// 	fmt.Println(*portFlag)
+	// }
+
+	port := flag.Int("port", 4000, "set the server port")
+	mode := flag.String("mode", "rest", "choose rest , html")
+
+	flag.Parse()
+
+	switch *mode {
+	case "rest":
+		rest.Start(*port)
+	case "html":
+		explorer.Start(*port)
+	default:
+		useage()
+	}
 }
